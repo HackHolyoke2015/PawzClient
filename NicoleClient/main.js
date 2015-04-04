920848d6250f9ba74099cdf85232b15a576766f7
Sunfloracat <nicolegossettm@gmail.com>
2015-04-04 09:40:43 -0400

    first commi dth

---
+++ NicoleClient/main.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+$(function(){
+	
+	var container = document.querySelector('#container');
+var msnry = new Masonry( container, {
+  // options
+  columnWidth: 200,
+  itemSelector: '.item'
+});
+
+
+});
